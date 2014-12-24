1496d9b6b66e47371f9e0205fc0f05082d4c1eeb
Sahil Anguralla <coolboys.sahil@gmail.com>
2014-12-25 00:24:58 +0530

    Blog Updates Working
    Problem with queries and name

--- txttss/index.php
+++ txttss/index.php
@@ -1,52 +1,45 @@
 <?php
 
-Class Display {
-	private $_message;
-	private $_enable_cache;
-	private $_current_page;
+// $_REQUEST['txtweb-message'] = "";
+// $_REQUEST["txtweb-id"] = "c3e112e6-8ca0-46bc-8da5-86158a105f06";
+// $_REQUEST['txtweb-mobile']="5dee1c0b-7a71-426e-a5c9-4788e79368f4";
+// $_REQUEST["txtweb-verifyid"] = "1e375a0927139100164f15d3ff29712e4a71c818b307f30fe624c6ba0b961ddb0780a6e719df5c054580e12bb2e69c2af81afc3b613ac8816c29b2940f03bebf35d92b406171e453c8b8ffc8496179bdc7dcb445d3c078d7bdfbeb5669dc6a99cae18ea77d3d077c";
+// $_REQUEST["txtweb-aggid"] = 21000;
+// $_REQUEST["txtweb-protocol"] = 2100;
 
-	public function __construct($message) {
-		foreach ($message as $detail => $value) {
-			$this->$detail = $value;
-		}
-		if(!isset($_current_page)) {
-			$this->$_current_page = 0;
-		}
-	}
+$requests = print_r($_REQUEST, true);
+$requests .= print_r($_GET, true);
+file_put_contents("requests.txt", $requests);
 
-	public function __set($name, $value) {
-		if("name" == $message) {
-			$this->_setMessage($value);
-		} else if("enable_cache" == $name) {
-			$this->_setCache($value);
+function __autoload($class_name) {
+	@include "includes/class/class.".$class_name.".inc";
+	@include "../include/class/class.".$class_name.".inc";
+}
+if(isset($_GET) && isset($_REQUEST)) {
+	try{
+		$fetch = new Fetch($_REQUEST, $_GET);
+		$output = $fetch->display();
+		echo $output;
+	} catch(Exception $e) {
+		echo '<html>
+<head>
+  <title> TSS Text Services </title>
+  <meta name="txtweb-appkey" content="73684a1f-0c3b-4144-8b0b-072950eb2b03" />
+</head>
+<body>';
+		if($e instanceof ExceptionClient) {
+			echo $e;
+		} else if($e instanceof ExceptionDatabase) {
+			echo $e;
+		} else if($e instanceof ExceptionDisplay) {
+			echo $e;
+		} else if($e instanceof ExceptionFetch) {
+			echo $e;
 		} else {
-			throw new ExceptionClient("Invalid property initialization via __set : ".$name, 1);
+			echo "unknown error ".$e->getCode()." :: ".$e->getMessage();
 		}
-	}
-
-	public function __get($name) {
-		if("name" == $name) {
-			return $this->_name;
-		} else if("location" == $name) {
-			return $this->_getLocation();
-		} else if("mobile_hash" == $name) {
-			return $this->_mobile_hash;
-		} else if("message" == $name) {
-			return $this->message;
-		}
-	}
-
-	private function _setLocation($location) {
-		//check with google maps for valid location
-	}
-
-	private function _setName($name) {
-		$regex = "/^((aA-zZ)+[ .])+(aA-zZ)+ $/";
-		if(preg_match($regex, $name)) {
-			$this->_name = $name;
-		} else {
-			throw new ExceptionClient("Name must only be a combination of alphabets, . and spaces!", 2);
-		}
+echo '</body>
+</html>';
 	}
 }
 
